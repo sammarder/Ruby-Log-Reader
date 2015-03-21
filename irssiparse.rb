0f1e9c1fe9d82aaaf1d54dc67e6545f6673168c0
Sam Marder <smardercollege@gmail.com>
2015-03-21 17:15:47 -0400

    added in some more optimization to the month code

--- irssiparse.rb
+++ irssiparse.rb
@@ -3,9 +3,32 @@
 # --- Log opened Sat Sep 06 18:28:43 2014
 #--- Day changed Sat Jan 25 2014
 #18:30 kubunto: but then again money is in streaming anyway
+
+MONTHS = {
+ '01' =>  'Jan',
+ '02' =>  'Feb',
+ '03' =>  'Mar',
+ '04' =>  'Apr',
+ '05' =>  'May',
+ '06' =>  'Jun',
+ '07' =>  'Jul',
+ '08' =>  'Aug',
+ '09' =>  'Sep',
+ '10' =>  'Oct',
+ '11' =>  'Nov',
+ '12' =>  'Dec',
+}
+
+def get_month(month)
+  if (MONTHS.has_key?(month)
+    return MONTHS[month]
+  else
+    puts "Whoops somethign is bad with this month"
+  end
+end 
+
 def parse(line)
   isUseful = false
-  puts @month
   if line[/\d\d:\d\d [a-zA-Z]/]
     puts "this line maches as a conversation line"
   elsif line[/--- Log/]
@@ -19,36 +42,5 @@ def parse_date(date)
     @month = get_month(values[0])
     @day = values[1]
     @year = values[2]
-  end
-end
-
-def get_month(month)
-  case month
-  when '01'
-    return 'Jan'
-  when '02'
-    return 'Feb'
-  when '03'
-    return 'Mar'
-  when '04'
-    return 'Apr'
-  when '05'
-    return 'May'
-  when '06'
-    return 'Jun'
-  when '07'
-    return 'Jul'
-  when '08'
-    return 'Aug'
-  when '09'
-    return 'Sep'
-  when '10'
-    return 'Oct'
-  when '11'
-    return 'Nov'
-  when '12'
-    return 'Dec'
-  else
-    puts 'Woops, that was bad'
   end
 end
