6010e0e1eb783a27f678002e5207c758643fe1d9
Sam Marder <smardercollege@gmail.com>
2015-03-22 15:11:30 -0400

    Refined the end case for playing the log file

--- file.rb
+++ file.rb
@@ -5,7 +5,7 @@ require_relative 'irssiparse'
 def read_file(filename, date)
   parse_date(date)
   File.open(filename, "r") do |infile|
-    while (line = infile.gets)
+    while (line = infile.gets && get_state)
       parse(line)
     end
   end
