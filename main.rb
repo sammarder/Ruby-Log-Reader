fa2fcd7a9f183dcc6f1aa4dae47bf486db709d10
Sam Marder <smardercollege@gmail.com>
2015-04-19 13:21:03 -0400

    Switched over to slop for command parsing

--- main.rb
+++ main.rb
@@ -1,20 +1,21 @@
 #!/usr/bin/ruby -w
 
 require_relative 'file_parser'
+require 'slop'
 
+fp = Fileparser.new
 if ARGV.length == 4
-  for i in 0..2 do
-    if ARGV[i] === '-f' || ARGV[i] === '-file'
-      filename = ARGV[i + 1]
-    elsif ARGV[i] === '-d' || ARGV[i] === '-date'
-      date = ARGV[i + 1]
-    end 
+  opts = Slop.parse do |o|
+    o.string '-f', '--file', 'a log file'
+    o.string '-d', '--date', 'a date'
   end
+  fp.read_file(opts[:file], opts[:date])
 else
   puts 'What file do you want to look into?'
   filename = gets.chomp
   puts 'What day do you want to look at? (MM/DD/YYYY)'
   date = gets.chomp
+  fp.read_file(filename, date)
 end
-fp = Fileparser.new
-fp.read_file(filename, date)
+
+
